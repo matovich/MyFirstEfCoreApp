5e9ff4e8635de90baf99b4de1a0b9f645deb0bfe
DESKTOP-KT5P20M\Paul <matovich@hotmail.com>
2018-08-13 23:05:29 -0500

    First data structure

---
+++ MyFirstEfCoreApp/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyFirstEfCoreApp
+{
+    public class AppDbContext : DbContext
+    {
+        private const string ConnectionString =
+            @"Server=(localdb)\mssqllocaldb; Database=MyFirstEfCoreDb; Trusted_Conneciton=True";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
--- MyFirstEfCoreApp/Program.cs
+++ MyFirstEfCoreApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace MyFirstEfCoreApp
 {
@@ -8,6 +9,19 @@ namespace MyFirstEfCoreApp
         {
             Console.WriteLine("Test");
 
+        }
+
+        public static void ListAll()
+        {
+            using (var db = new AppDbContext())
+            {
+                foreach (var book in db.Books.AsNoTracking().Include(a => a.Author))
+                {
+                    var webUrl = book.Author.WebUrl == null ? "- no web URL biven -" : book.Author.WebUrl;
+                    Console.WriteLine($"{book.Title} by {book.Author.Name}");
+                    Console.WriteLine($"   Published on {book.PublishedOn:dd-MMM-yyyy}. {webUrl}");
+                }
+            }
         }
     }
 }
